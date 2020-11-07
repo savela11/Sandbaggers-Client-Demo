a87c86b588f8bb768f0cf7dde80f47632c5a2bd0
Andy Savela <savela11@gmail.com>
2020-11-06 19:45:37 -0500

    update media queries

--- src/components/navigation/NavBar.vue
+++ src/components/navigation/NavBar.vue
@@ -2,13 +2,17 @@
   <div class="nav">
     <div class="nav__bar">
       <div class="nav__bar--favorites">
-        <button>fav1</button>
-        <button>fav2</button>
-        <button>fav3</button>
+        <router-link v-for="link in favoriteLinks" :key="link.link" :to="link.link">
+
+          <img :src="favoriteLinkImg(link.name)" alt="">
+          <span>{{ link.name }}</span>
+        </router-link>
       </div>
       <div class="nav__bar--menu">
-        <button class="toggleMenuBtn" @click="toggleNavMenu"><img src="@/assets/icons/bottomBar-menu.svg" alt="menu" /></button>
-        <button class="hideNavBarBtn" @click="hideNavBar"><img src="@/assets/icons/hideEye.svg" alt="hide navbar red eye." /></button>
+        <button class="toggleMenuBtn" @click="toggleNavMenu"><img src="@/assets/icons/bottomBar-menu.svg" alt="menu" />
+        </button>
+        <button class="hideNavBarBtn" @click="hideNavBar">
+          <img src="@/assets/icons/hideEye.svg" alt="hide navbar red eye." /></button>
       </div>
     </div>
   </div>
@@ -18,6 +22,7 @@
 import { Component, Vue, Prop } from 'vue-property-decorator'
 import { ICurrentUser } from '@/types/User/AuthUser'
 import UIHelper from '@/utility/UIHelper'
+import { IFavoriteLink } from '@/types/User/User'
 
 @Component
 export default class Navigation extends Vue {
@@ -26,6 +31,46 @@ export default class Navigation extends Vue {
   show = false
 
   top = false
+
+  get favoriteLinks(): IFavoriteLink[] {
+    if (this.currentUser.settings.favoriteLinks.length > 0) {
+      return this.currentUser.settings.favoriteLinks
+    } else {
+      return [
+        { link: '/dashboard', name: 'Dashboard' },
+        { link: '/bets', name: 'Bets' },
+        { link: '/powerRankings', name: 'PowerRankings' }
+      ]
+    }
+  }
+
+  favoriteLinkImg(name: string): string {
+    console.log(name);
+    let svgImg
+    switch (name) {
+      case 'Dashboard':
+        svgImg = 'dashboard'
+        break
+      case 'MockDrafts':
+        svgImg = 'mockDraft'
+        break
+      case 'Ideas':
+        svgImg = 'ideas'
+        break
+      case 'Events':
+        svgImg = 'golf'
+        break
+      case 'PowerRankings':
+        svgImg = 'powerRankings'
+        break
+      case 'Bets':
+        svgImg = 'bets'
+        break
+      default:
+        svgImg = 'golf'
+    }
+    return require(`@/assets/icons/navMenu/${svgImg}.svg`)
+  }
 
   hideNavBar(): void {
     UIHelper.ToggleNavBar(false)
