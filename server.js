dc2e76e25318ecb0b321e140ba5690fcbdff8fc2
Andy Savela <savela11@gmail.com>
2020-11-14 13:18:16 -0500

    add express server

---
+++ server.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const express = require('express');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const serveStatic = require("serve-static")
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 80;
+app.listen(port);
